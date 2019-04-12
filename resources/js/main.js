6295f4243a1ae8c00d9bf0f0735bba0b018b4b8a
Mona Lisa <claudia2_rago@yahoo.com>
2019-04-11 22:35:30 -0400

    carousel working. Content not centered

---
+++ resources/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    accessibility: false,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+   pauseOnHover: false,
+   focusOnSelect: false,
+   draggable: false
+  });
+});
